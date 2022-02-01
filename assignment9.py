2b4db3d93089ef6426f49682ca04d499af2d5700
chiraggopani1207 <95518105+chiraggopani1207@users.noreply.github.com>
2022-02-01 22:13:37 +0530

    Add files via upload

---
+++ assignment9.py
@@ -0,0 +1,24 @@
+"""1. To what does a relative path refer?
+A= relative path is present working directory where your file is stored"""
+""" 2. What does an absolute path start with your operating system?
+A =absolute path starts with c: my current absolute path is :C:\Users\chira\PycharmProjects\INEURON"""
+"""3. What do the functions os.getcwd() and os.chdir() do?
+A =os.getcwd() is to know the current working directory and os.chdir() is used to change current working directory"""
+"""4. What are the . and .. folders?
+A = . represents the absolute path while .. signifies parent folder of current directory"""
+"""5. In C:\ bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
+A=C:\ bacon\eggs this is directory name and spam.txt is base name"""
+"""6. What are the three “mode” arguments that can be passed to the open() function?
+A = in file handling open(file name,mode) is syntex for calling a file
+r = is read mode
+w = is write mode
+a = append mode to add new content to existing content in file
+r+ = read + write mode to do simultaneous work
+x = for creating exclusiv new file """
+"""7. What happens if an existing file is opened in write mode?
+A = existing file will be overwritten in this mode and if no file exist then new file will be created"""
+"""8. How do you tell the difference between read() and readlines()?
+A = read() is used to read the whole file from starting to end
+readlines() will read all lines in file inside list"""
+"""9. What data structure does a shelf value resemble?
+A= is resembles key and value pair similar to dictionary"""
